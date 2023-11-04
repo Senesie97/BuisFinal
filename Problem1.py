57280d6064de285bacdef883eb7db5f2057df831
Senesie Sannoh <Sannohs0506@Students.bowiestate.edu>
2023-11-03 23:25:54 -0400

    Initial commit

---
+++ Problem1.py
@@ -0,0 +1,6 @@
+import math
+
+length = float(input('Enter length'))
+breadth = float(input('Enter breadth'))
+area = length * breadth
+print(area)
---
+++ problem3.py
@@ -0,0 +1,11 @@
+import math
+
+number1 = input('enter number 1')
+number2 = input('enter number 2')
+number3 = input('enter number 3')
+number4 = input('enter number 4')
+number5 = input('enter number 5')
+sum= int(number1) + int(number2) + int(number3) + int(number4) + int(number5)
+average = sum / 5
+print(sum)
+print(average)
